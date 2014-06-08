7ff3f4d968a65be6b1dd2f7fa3e9418ff0f0586e
Herald Yu <yuhr123@gmail.com>
2014-06-08 14:29:13 +0800

    add install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#copy inti script to syttem server
+echo "Initialization..."
+cp ./nginx /etc/init.d/
+chmod +x /etc/init.d/nginx
+
+#Installation dependent libraries
+echo "Depends..."
+apt-get install -y libpcre3-dev zlib1g-dev libcurl4-openssl-dev libssl-dev
+
+#Set Startup Items
+echo "StartUp..."
+update-rc.d -f nginx defaults
+
+#Display Nginx Version
+echo "The current version is:"
+/usr/local/nginx/sbin/nginx -v
